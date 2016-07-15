28fc667f74858ebaeff8321f5a54b99700568a8a
David Dawson <david.dawson@simplicityitself.com>
2016-07-15 15:06:16 +0100

    First commit

---
+++ test/test/example-test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+
+describe("example tests", function () {
+
+    this.timeout(30000);
+
+    before(function () {
+
+    });
+
+    after(function () {
+
+    });
+
+    it("do stuff", function (done) {
+      done()
+    });
+});
